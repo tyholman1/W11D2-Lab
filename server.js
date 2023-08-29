85e1ca86e613552ad028d3ed1c3232eb1b52f960
ty Holman <tyholman1@gmail.com>
2023-08-29 11:17:39 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/greeting/:name', (request, response) =>{
     response.send("<h1>What's up</h1>" + request.params.name)
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    res.send("<h1>Your tip is:</h1>" + (req.params.total)*(req.params.tipPercentage/100))
+})
+
 app.listen(3000, ()=>{
     console.log("Greets has been initiated")
 })
+
